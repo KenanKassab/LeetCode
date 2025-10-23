d3e98f48c0207ae789b9120dc3b03f6c6b0ea1ec
Kenan Kassab <kenankassab1997@gmail.com>
2025-10-24 00:23:34 +0300

    updated

---
+++ Easy/13 - Roman to Integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        order = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+        last_order = 1000
+        ans = 0
+        for i in range(len(s) - 1):
+            ch = s[i]
+            print(i, ch)
+            print(s[i + 1])
+            if order[ch] < order[s[i + 1]]:
+                ans -= order[ch]
+            else:
+                ans += order[ch]
+        ans += order[s[-1]]
+        print(ans)
+        return ans
