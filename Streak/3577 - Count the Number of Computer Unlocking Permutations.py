9d3712161e69c4f32ac4d8e3dcb7391c34f29eee
Kenan Kassab <kenankassab1997@gmail.com>
2025-12-10 17:29:21 +0300

    updated

---
+++ Streak/3577 - Count the Number of Computer Unlocking Permutations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPermutations(self, complexity: List[int]) -> int:
+        if complexity[1] <= complexity[0]:
+            return 0
+        MOD = 10**9 + 7
+        n = len(complexity)
+        for i in range(1,n):
+            c = complexity[i]
+            if c <= complexity[0]:
+                return 0
+
+        
+        ans, mod = 1, 10**9 + 7
+        for i in range(2, n):
+            ans = ans * i % mod
+        return ans
